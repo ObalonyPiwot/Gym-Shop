5dfa922f088ac1ccd1e110672a5af892becaefa8
moszynMTS <95504374+moszynMTS@users.noreply.github.com>
2023-03-27 20:34:50 +0200

    Add files via upload

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,9 +3,12 @@ import React from "react";
 function Sidebar(){
     return(
         <div className="sidebar">
-            <li><a href="index.html">Suplementy</a></li>
+            {/* <li><a href="index.html">Suplementy</a></li>
             <li><a href="index2.html">Akcesoria</a></li>
-			<li><a href="index3.html">Sprzęt</a></li>
+			<li><a href="index3.html">Sprzęt</a></li> */}
+            <a href="index.html">Suplementy</a>
+            <a href="index2.html">Akcesoria</a>
+			<a href="index3.html">Sprzęt</a>
         </div>
     )
 }
