c8cc1b255b60f471ab57d547376ecb1ba1749eb0
MrMarioo <MariFish@o2.pl>
2023-04-01 13:26:29 +0200

    InsertGenerator

--- SQL/inserty.sql
+++ SQL/inserty.sql
@@ -10,4 +10,67 @@ create table uzytkownik(
     Telefon NVARCHAR2(9)
 );
 
-INSERT INTO uzytkownik VALUES(UzytkownikID.nextVal, 'Imie','Nazwisko','Admin','login','haslo','email@gmail.com', '123456789');
+
+
+INSERT INTO uzytkownik VALUES (UzytkownikID.nextVal, 'Beatrice', 'Forconi', 'Uzytkownik', 'enopa', '2f1cc83c', 'olugok@wulgakug.mv', '(914) 670-3058');
+INSERT INTO uzytkownik VALUES (UzytkownikID.nextVal, 'Lora', 'Wong', 'Uzytkownik', 'ukoor', '4c4e2710', 'lu@ka.net', '(501) 767-1266');
+INSERT INTO uzytkownik VALUES (UzytkownikID.nextVal, 'Bertha', 'Matthews', 'Uzytkownik', 'esamu', '3095e1ed', 'vuna@akaso.jp', '(541) 614-3797');
+INSERT INTO uzytkownik VALUES (UzytkownikID.nextVal, 'Adele', 'Nguyen', 'Admin', 'nedko', '029178ea', 'javihu@rodakci.cn', '(635) 449-9639');
+INSERT INTO uzytkownik VALUES (UzytkownikID.nextVal, 'Daisy', 'Owen', 'Uzytkownik', 'amko', '92ccb39b', 'jahuhub@colic.as', '(983) 328-4162');
+INSERT INTO uzytkownik VALUES (UzytkownikID.nextVal, 'Lydia', 'Pugliese', 'Uzytkownik', 'hufsaz', '130899d2', 'ibonuw@loz.dk', '(459) 260-7507');
+INSERT INTO uzytkownik VALUES (UzytkownikID.nextVal, 'Delia', 'Newman', 'Admin', 'zuwu', '652bc2da', 'itifo@ekukani.ke', '(981) 392-1034');
+INSERT INTO uzytkownik VALUES (UzytkownikID.nextVal, 'Blake', 'Magrini', 'Admin', 'jobi', '64c5df27', 'sihvoguv@fu.edu', '(511) 998-1307');
+INSERT INTO uzytkownik VALUES (UzytkownikID.nextVal, 'Claudia', 'Arnetoli', 'Uzytkownik', 'zuroj', 'ff9dc740', 'iziodumi@pajtog.tv', '(986) 203-1486');
+INSERT INTO uzytkownik VALUES (UzytkownikID.nextVal, 'Carolyn', 'Moya', 'Admin', 'mehe', '200a60eb', 'fatud@silozos.mg', '(321) 787-8414');
+INSERT INTO uzytkownik VALUES (UzytkownikID.nextVal, 'Roy', 'Goodman', 'Uzytkownik', 'jegut', '01833bb5', 'loviste@gupodki.gl', '(780) 902-8790');
+INSERT INTO uzytkownik VALUES (UzytkownikID.nextVal, 'Keith', 'Page', 'Uzytkownik', 'zina', '3986221d', 'vim@hek.tp', '(867) 742-1750');
+INSERT INTO uzytkownik VALUES (UzytkownikID.nextVal, 'Verna', 'Rey', 'Admin', 'ehohe', '0c9822ea', 'ba@ivzaza.fm', '(331) 228-5197');
+INSERT INTO uzytkownik VALUES (UzytkownikID.nextVal, 'Tillie', 'Perini', 'Uzytkownik', 'cairo', '8c9c5ec0', 'erzugoc@mohabo.my', '(850) 413-5491');
+INSERT INTO uzytkownik VALUES (UzytkownikID.nextVal, 'Claudia', 'Kennedy', 'Uzytkownik', 'gata', '3b232e7e', 'vibgoezu@sil.ms', '(513) 953-2736');
+INSERT INTO uzytkownik VALUES (UzytkownikID.nextVal, 'Ernest', 'Braschi', 'Uzytkownik', 'lohhiw', '63e7684e', 'ritobeb@vegduv.fi', '(579) 458-7469');
+INSERT INTO uzytkownik VALUES (UzytkownikID.nextVal, 'Dennis', 'Cox', 'Uzytkownik', 'pulum', '9b2443ae', 'imujda@sej.np', '(682) 816-7196');
+INSERT INTO uzytkownik VALUES (UzytkownikID.nextVal, 'William', 'Guarnieri', 'Uzytkownik', 'degnuc', 'bef429ce', 'neihiuf@wu.kp', '(475) 842-5159');
+INSERT INTO uzytkownik VALUES (UzytkownikID.nextVal, 'Olive', 'Cooper', 'Uzytkownik', 'zorvi', '99c19e6d', 'siivoref@amu.bt', '(310) 604-6594');
+INSERT INTO uzytkownik VALUES (UzytkownikID.nextVal, 'Amelia', 'Wolfe', 'Uzytkownik', 'votje', 'a99f5348', 'pojebti@ni.gs', '(957) 900-5523');
+INSERT INTO uzytkownik VALUES (UzytkownikID.nextVal, 'Jean', 'Nixon', 'Admin', 'kugmid', '838ac680', 'go@vafeknud.sh', '(846) 614-9042');
+INSERT INTO uzytkownik VALUES (UzytkownikID.nextVal, 'Brandon', 'Goretti', 'Uzytkownik', 'hehim', '43d33afe', 'gar@bizsu.bw', '(607) 647-7001');
+INSERT INTO uzytkownik VALUES (UzytkownikID.nextVal, 'Violet', 'Tacconi', 'Admin', 'petiv', 'ebf892aa', 'la@cir.gov', '(773) 659-1284');
+INSERT INTO uzytkownik VALUES (UzytkownikID.nextVal, 'Cole', 'Carroll', 'Uzytkownik', 'niro', 'c821aa39', 'ekegiv@wuojiafe.qa', '(531) 979-3308');
+INSERT INTO uzytkownik VALUES (UzytkownikID.nextVal, 'Marcus', 'Facchini', 'Admin', 'nemde', '7808c68b', 'lu@ez.ar', '(453) 384-3522');
+INSERT INTO uzytkownik VALUES (UzytkownikID.nextVal, 'Gerald', 'Cohen', 'Uzytkownik', 'vias', 'f9204c08', 'es@gaf.dk', '(841) 344-8523');
+INSERT INTO uzytkownik VALUES (UzytkownikID.nextVal, 'Miguel', 'Cencetti', 'Uzytkownik', 'tussen', '7abf0673', 'toorneh@liw.al', '(533) 911-1755');
+INSERT INTO uzytkownik VALUES (UzytkownikID.nextVal, 'Maude', 'Betti', 'Uzytkownik', 'vepjoc', 'cb10e997', 'notgenfaz@mi.gl', '(708) 776-4260');
+INSERT INTO uzytkownik VALUES (UzytkownikID.nextVal, 'Fanny', 'Boulanger', 'Admin', 'evekat', '8d53cf4a', 'zettirul@dad.ar', '(216) 351-3872');
+INSERT INTO uzytkownik VALUES (UzytkownikID.nextVal, 'Nelle', 'Williams', 'Uzytkownik', 'midir', '63f59812', 'lagiboli@ogunodaw.id', '(354) 495-6281');
+
+
+INSERT INTO produkty VALUES ( produktyID.nextVal, 'Białko serwatkowe', 'Joabge re adocizo tocoh juih vima nihiunu ig uwtom mep ido lab. Tucel fu ruvef helpemev gezu sover ohenuwuz efoko tuzcal vefhiz goh ojevehvi nonem nizpife utpesu numbi cakci.', 151.38, 1);
+INSERT INTO produkty VALUES ( produktyID.nextVal, 'Kreatyna', 'Kakzaw cidapavof lopekrab vacam ge agabaej hutfibej tigrag jisni hatsuj bogucve atditgu od hezmujo datuvget. Zov ocbi fewrok mot tafusewe honpefovo hejso fihweg vevhuri tumepvi tor ini kevbut efhegjar.', 241.88, 5);
+INSERT INTO produkty VALUES ( produktyID.nextVal, 'BCAA', 'Cuhahemi wuvge mivael guba kedifwen onice vez gainevur fel pidub raj ire engacfem bojoti vivin apono sovazi dulpidoma. Geget sahog sakje kafloz ribnif va opgejme dihlevhor petbipib ob pivophi ehle gahet fuloewo somda zat or cenpagur.', 521.75, 4);
+INSERT INTO produkty VALUES ( produktyID.nextVal, 'Glutamina', 'Ce kiig te fo umo wo bake etve kazor nul ujoziw jebuuz uc. Ces jotzat pawavu oki ukoez jo ho gocewgav hu firke izuve ruw sijbe.', 457.55, 5);
+INSERT INTO produkty VALUES ( produktyID.nextVal, 'Pre-workout', 'Us wices timefec wabrawnep id suhfozav egej jehalile bas mimho ofluh bacoiji as accirak afoka du. Anufza erci hamem za mijocoje epa huokwi mev cuh naskowa tozeeju golvoule fi akagaiz asezapa.', 646.25, 5);
+INSERT INTO produkty VALUES ( produktyID.nextVal, 'Witaminy i minerały', 'Oscar tanlum hedtir ubluzip raodi om cajeg dereb tek arojufozo ejetu viwuda utfa me. Rof pek vahgef dat viafika ewajila fe micbaob nodlutwi bojovo numiva foictog.', 53.79, 4);
+INSERT INTO produkty VALUES ( produktyID.nextVal, 'Tłuszcz omega-3', 'Kupduzaf nocgi jic nafarhas emisifup pab aw akaisbaw tep kaupivu hupro cejiplo duz vobewib demewi. Valir uboofna religaw ned pevugu uc ruknaef katusaf zak ofugiple ve ja doahuser mi zutpovjuk.', 672.1, 5);
+INSERT INTO produkty VALUES ( produktyID.nextVal, 'Żelazo', 'Nalid luvabov rucahe kohim jehiwki amuoveige adpa osadof oci egkoclu asi dibira il folimuca. Kum ojgema aso ebibif lied lor fen udiwooj bares iguno ki somvap wiivcav ta ruj.', 942.31, 2);
+INSERT INTO produkty VALUES ( produktyID.nextVal, 'Butelka na wodę', 'Ni vukav tu bovimpel bo mobhopvec isu ho nuni butki ruerko tupru li gizzok. Curu bi feig nesagdo faroviv nebe socujcum afurituh irazinis rubuifi soz afufo ceznuc.', 702.93, 2);
+INSERT INTO produkty VALUES ( produktyID.nextVal, 'Ręcznik treningowy', 'Raejik muldo esola pajepuh tisozi hotwu cunu urelimez ob wuutoes zogatu gusorom vuv mohmur obduho bemwak vojarato co. Gi umeetjo ukpibfuf fuco suarvim nevduguv ril vikonuv ge raaw donitafi ijubaib irci.', 74.63, 5);
+INSERT INTO produkty VALUES ( produktyID.nextVal, 'Zestaw sztangielek', 'Horma has vijebmur lez figpusne kaweh vegemu betij gaoco imu mamre do jamcuneta kuafa doboad owhirho kuzus jigrijcu. Met ulenepuni tum visadadu ekhu ero nud weriv wesjajto wirefel zawuz bummutkeb dogasaog ifkedfi asibuaz kuagha me.', 992.3, 4);
+INSERT INTO produkty VALUES ( produktyID.nextVal, 'Mata treningowa', 'Nan hugokze vin kiclornub juva so wivic kimcu ze lazonilo godujiow govsuce hovagu pera. Wu uw mipeg kahu jigiko ka alho cunzadces vorisub inidalwus tedboni ev urasul po up be nedol owa.', 976.14, 5);
+INSERT INTO produkty VALUES ( produktyID.nextVal, 'Pas treningowy', 'As zuida ozukidwif elnik bebawsa utakokefu riwafu tu ako tagus zaraj de jihezi saz. Ve pelafi rickicpe wofuku seviju wes ucpus igmofen ekoh sup lubowfu pofciwu tikpeoru ror kamto ivujefun baiwoce kikarwe.', 224.04, 1);
+INSERT INTO produkty VALUES ( produktyID.nextVal, 'Rękawice treningowe', 'Pa bauhvif wop rovkebgav put dof vun ubwo ruhavu niwapin orzab pe zuwte nekase cesepe. Okojuuzo koj pave ridav upsibra nakadifof je ij raco uretanif mejvi niowtub aseafu ves ca luijecah vodasgi diwsasrig.', 78.78, 3);
+INSERT INTO produkty VALUES ( produktyID.nextVal, 'Paski treningowe', 'Osok ir um inpomlis ran tuzat gabpeg vofada nijoz zazfego tafpat gaw. Hugiduw cihebi sejuwmez heowu zubotaz zenor rukurkir lef hijvo nizecivu zomku oppalgo wopavus hefa.', 67.85, 2);
+INSERT INTO produkty VALUES ( produktyID.nextVal, 'Chusta na szyję', 'Papraz jirre ibano cuopim dati nombipik litiher ijosa cogelaow ju atulep luc nikow rewa lucah. Maderbak utigirus hizkefil guh han egiwuro ogetuitu diecihig tevge nuz se sa sagupuj gossiribi zubcovi boseezu oco arnuwewe.', 730.4, 5);
+INSERT INTO produkty VALUES ( produktyID.nextVal, 'Szelki na ciężary', 'Su zuluge pevvizu nac toc beesuda citop padajok go ojnoos bu nonucho jil abe pomundaf wapabzud. Ilte ehodud pigwal cead ga kap dodepaho giwidhif cev dehfokten juse gasgew.', 316.44, 3);
+INSERT INTO produkty VALUES ( produktyID.nextVal, 'Pas do podciągania', 'Su temo bub nod pafiw du mup bopehe fipot fukrun so pak ovoudep kocaku we. Ze hikmatez fe bozfunon icoretus cina hej dahin nieha ut kot ize itujasej dugak kikgahga ho.', 112.47, 5);
+INSERT INTO produkty VALUES ( produktyID.nextVal, 'Pompki', 'Ziwnu nopdewom jejre uw eczuwjo tuv ehge ez mup nesoiro depgabse vucnelaw aca in zij va. Gi igir siati razur cuaggos jouzuuj dozuwkot kerav olnobgat ewetuhtim puenoof zisise fewevrok vebholsew son vafnup jof.', 11.41, 5);
+INSERT INTO produkty VALUES ( produktyID.nextVal, 'Drążek do podciągania', 'We ga if dij bevzuhzus ilutizef wuz cacga cohol lo opo ugusan. Ofisug kaliza ec otu natal ziri openombup sor ka puhoece fubdojle lawehaho.', 750.05, 4);
+INSERT INTO produkty VALUES ( produktyID.nextVal, 'Hantle regulowane', 'Nibwean cop asu rujbejog bin lucirce utakoneh mad kojfuhos datso udfot so nupi siskuwco. Usica ehevecice risfej juse ta ladone men jebhizik bo ap balhaf zon onu abuca okuwud abozeak ricu.', 206.22, 3);
+INSERT INTO produkty VALUES ( produktyID.nextVal, 'Bieżnia elektryczna', 'Velmehafu uknenuv demrilis jesogho vek at mej sa rukuze ekmiko deruda jubu po forun bugu rusackot holol ten. Hutive tara vuwu kize fo je urucoak fazan ti lo pofsuile dikuce jo vomutaz ampuhise iku muatune fu.', 968.03, 3);
+INSERT INTO produkty VALUES ( produktyID.nextVal, 'Rowerek stacjonarny', 'Asoeriimo goezobe ocirezdic sa aw poh buominer so mirurne mur ihbuhhoc ebju etalo tivdiju oseuzto. Dedizod sac anoraor dikje nizabtu huwgik re vibnojup lojaf mog tiz fubsafu nosnijhug zate.', 454.44, 1);
+INSERT INTO produkty VALUES ( produktyID.nextVal, 'Orbitrek', 'Riosuiz suw pevu fero kur hofuvek uvi powev culad mi cantam tefge vem dopi ogfef ud. Jicatapo vi ca ziw go ib cefeblit arosuno bug uguvej rusven hofodul jo.', 929.96, 4);
+INSERT INTO produkty VALUES ( produktyID.nextVal, 'Platforma do podnoszenia ciężarów', 'Tip ar nigibe sewid gal mem vah ho je iritacniw gat um me diw cab atase. Nib ziumu zuv hu nocoso det ejukiv johasi cepco wireviz uzite bi finsihjes icuji agnok izesin iw siew.', 753.07, 4);
+INSERT INTO produkty VALUES ( produktyID.nextVal, 'Skakanka', 'Razitwa sunmoji agora dimje ulo hizupzo sojgev hiz to ha jollad siteswu neohsah fe disosuen rahcaccel jofenacet uju. Varowifi funpi vozi tulegak ceuzihaw ne ugu femko lu wululi uhcap obutef.', 609.42, 3);
+INSERT INTO produkty VALUES ( produktyID.nextVal, 'Piłka do ćwiczeń', 'Uf ro lug ja jol zelsivgum bas dombatuwo obiatohiv nob ruk cih cerroljih ted. Utaka wafega kukenzuz min pebachov ki vopo paczahli mogojmo do gewhop joloh juw wenoahe vup jivbonit.', 798.93, 1);
+INSERT INTO produkty VALUES ( produktyID.nextVal, 'Worki treningowe', 'Fi uwjav givit diw ho hakoca suvnoj ma se hiwi zacim ba biziwuj cek lanicis. Irig mefuop vos ubuzelo eglel jor binhar loncuk sagez noluw hihruwru fiundad umopuuka za bo dul co.', 655.19, 1);
+INSERT INTO produkty VALUES ( produktyID.nextVal, 'Rower treningowy', 'Fega po imozoos ofo tubhil nopogiul girjih ejuleud got unga pevtuzo akuji oc levlam ziboba pagu gapuski hodwusek. Poprenof puuca acamed tija ucipaczel cap mi ejiranzor imutabog vow zi gep mop sojfafuv hoegi.', 807.18, 3);
+INSERT INTO produkty VALUES ( produktyID.nextVal, 'Zestaw do wspinaczki', 'Rogod ewa hetgonup dasa voremi da ula ik lekjawzon awonactes fev ive rurujfew so siwar fowmiweh gucesci. Uzdus wuvenus weannat wi hesvavse hisikpam ma rizmizef bedgitere tilir wipeno wehet ho tuljizu.', 701.81, 4);
