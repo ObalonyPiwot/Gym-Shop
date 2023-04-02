a7a5ad7593829863f12cffcb59179cfb35453bdc
ObalonyPiwot <74457736+ObalonyPiwot@users.noreply.github.com>
2023-04-02 18:44:17 +0200

    little login fix

--- GymShopSpring/src/main/java/pl/gymshopspring/Controller.java
+++ GymShopSpring/src/main/java/pl/gymshopspring/Controller.java
@@ -1,6 +1,7 @@
 package pl.gymshopspring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,10 +24,27 @@ public class Controller {
 
     @GetMapping("/login/{login}/{password}")
     public String login(@PathVariable("login") String login,@PathVariable("password") String password) throws SQLException {
-        String sql = "Select haslo from uzytkownik where login = "+login;
+        String sql = "Select haslo from uzytkownik where login = '" + login + "'";
         System.out.println(sql);
-        
-        String data = (String) jdbc.queryForObject(sql, new Object[] { }, String.class);
-        return "{\"Status\":\""+data+"\"}";
+
+        try {
+            String data = (String) jdbc.queryForObject(sql, new Object[]{}, String.class);
+            return "{\"Status\":\"" + data + "\"}";
+        } catch (EmptyResultDataAccessException e) {
+            return "{\"Status\":\"error\",\"Message\":\"User not found\"}";
+        } catch (Exception e) {
+            return "{\"Status\":\"error\",\"Message\":\"" + e.getMessage() + "\"}";
+        }
+    }
+
+    @GetMapping("/register/{login}/{password}/{email}/{imie}/{nazwisko}/{telefon}")
+    public String login(@PathVariable("login") String login,@PathVariable("password") String password,
+                        @PathVariable("email") String email,@PathVariable("imie") String imie,
+                        @PathVariable("nazwisko") String nazwisko,@PathVariable("telefon") String telefon) throws SQLException {
+        String sql = "INSERT INTO uzytkownik VALUES(UzytkownikID.nextVal, '"+imie+"','"+nazwisko+"','Klient','"+login+"','"+password+"','"+email+"', '"+telefon+"')" ;
+        System.out.println(sql);
+
+        jdbc.update(sql);
+        return "{\"Status\":\"registered\"}";
     }
 }
