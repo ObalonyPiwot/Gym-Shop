8da75c5b2196a9c06accfb5cc88db59872112689
MrMarioo <MariFish@o2.pl>
2023-04-05 17:58:19 +0200

    commit Szyma bożego

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,18 +1,52 @@
-//const BlogList = (props) => {
-const BlogList = ({blogs, title}) => {
-    //const blogs = props.blogs;
-    //const title = props.title;
+import PreviewBlog from "./PreviewBlog";
+
+const BlogList = ({blogs, title, childToParent}) => {
+    
     return (
-    <div className="blogList">
-        <h2>{ title }</h2>
-            {blogs.map((blog) => (
-                <div className='blogPreview' key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>body {blog.body}</p>
-                    <p>author {blog.author}</p>
-                </div>
-            ))}
+
+        <div className="blogList">
+            <div className="blogTitleDiv">         
+                <div className="blogTitle">
+                        <h2>{title}</h2>
+                </div> 
+            </div>
+            <div className="blogPreviewDiv">
+                
+                {blogs.map((blog) => {
+                    const photo = blog.photo;
+                    let src = require(`${photo}`);
+                    return (
+                        <a className='blogPreview' key={blog.id} onClick={() => childToParent(blog)}>
+                            <div class="image">
+                                <img src={src} alt={blog.title} />
+                            </div>
+                            <div class="text">
+                                <div class="top">
+                                    <p>{blog.title}</p>
+                                </div>
+                                {/* <div class="middle"> Opis
+                                    <p>{blog.body}</p>
+                                </div> */}
+                                <div class="bottom">
+                                    <table>
+                                        <tr>
+                                            <th> <p className="cena">Cena: {blog.cena}</p></th>
+                                        </tr>
+                                        <tr>
+                                            <th className="guzior"> <button>Pokaż więcej</button></th>
+                                        </tr>
+                                        {/* <tr>
+                                            <th> <p className="author">Producent: {blog.author}</p></th>
+                                        </tr> */}
+                                    </table>
+                                </div> 
+                            </div>
+                        </a>
+                    )
+                })}
+            </div>
         </div>
-     );
+    );
 }
+
 export default BlogList;
