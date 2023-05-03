9891e99751d4fcb95ea5b4dab190d7aa4b74005d
ObalonyPiwot <74457736+ObalonyPiwot@users.noreply.github.com>
2023-05-03 22:23:27 +0200

    Add files via upload

--- SQL/create.sql
+++ SQL/create.sql
@@ -34,7 +34,6 @@ create table uzytkownik(
     Imie NVARCHAR2(50),
     Nazwisko NVARCHAR2(50),
     TypKonta NVARCHAR2(50),
-    Login NVARCHAR2(50),
     Haslo NVARCHAR2(50),
     Email NVARCHAR2(50),
     Telefon NVARCHAR2(9)
